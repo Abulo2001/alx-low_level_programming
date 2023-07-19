719b583e3260a4163b8e935f88c22e0fdaf3585c
Abulo2001 <piuschineme6@gmail.com>
2023-07-19 02:26:37 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
 * _isalpha - function that checks for alphabetic character
-* @c - character to be checked
+* @c:  character to be checked
 * Return: 1 if letter, lower or uppercase, zero otherwise
 */
 
