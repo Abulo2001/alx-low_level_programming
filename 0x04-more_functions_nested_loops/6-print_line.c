a4b414d6669532e26038b36b6e622ab713c8d83f
Abulo2001 <piuschineme6@gmail.com>
2023-07-21 15:46:03 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_line -function that prints a straight line
+  * @n: amount of times to print
+  */
+
+void print_line(int n)
+{
+int a = 1;
+
+for (; a <= n && n >= 1)
+{
+_putchar('_');
+}
+_putchar('\n);/*new line*/
+}
