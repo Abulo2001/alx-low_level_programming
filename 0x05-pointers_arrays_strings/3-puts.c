a46120ef25033fee8fc805dad50a6cf0bc18baca
Abulo2001 <piuschineme6@gmail.com>
2023-07-24 16:25:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  * _puts - function that prints a string, followed by a new line
+  * @str: a pointer argument for string
+  */
+
+void _puts(char *str)
+{
+puts(str);
+}
