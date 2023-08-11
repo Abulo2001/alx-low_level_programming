46f906ca7b44c428e7c9fc05b2b6947c82b85263
Abulo2001 <piuschineme6@gmail.com>
2023-08-11 01:41:37 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+int **allow_grid(int width, int height);
+void free_grind(int **grid, int height);
+#include <stdio.h>
+#include <stdlib.h>
